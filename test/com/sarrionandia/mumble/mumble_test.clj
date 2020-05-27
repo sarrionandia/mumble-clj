e504c87a9594e5d255d4752f3eb475d3274f18ca
Tito Sarrionandia <r@sarrionandia.com>
2020-05-27 14:41:30 +0100

    Test for single uppercase letter

--- test/com/sarrionandia/mumble/mumble_test.clj
+++ test/com/sarrionandia/mumble/mumble_test.clj
@@ -6,8 +6,11 @@
   (testing "Empty string returns empty string"
     (is (= "" (m/mumbler ""))))
 
-  (testing "Single letter a"
+  (testing "Single letter a returns capitalised A"
     (is (= "A" (m/mumbler "a"))))
 
-  (testing "Single letter b"
-    (is (= "B" (m/mumbler "b")))))
+  (testing "Single letter b returns capitalised B"
+    (is (= "B" (m/mumbler "b"))))
+
+  (testing "Single letter B is unmodified"
+    (is (= "B" (m/mumbler "B")))))
