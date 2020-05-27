55f4ff80ebbcdacd7c756f08ee0f982c5adb9d29
Tito Sarrionandia <r@sarrionandia.com>
2020-05-27 15:08:42 +0100

    Refactor: Mumble letter by letter:

--- src/com/sarrionandia/mumble/mumble.clj
+++ src/com/sarrionandia/mumble/mumble.clj
@@ -1,9 +1,10 @@
 (ns com.sarrionandia.mumble.mumble)
 
+(defn- mumble-letter [letter position]
+  (clojure.string/upper-case letter))
+
 (defn mumbler [x]
-  (let [char-seq (seq x)]
+  (let [char-seq (vec x)]
     (if (= 0 (count char-seq))
       ""
-      (clojure.string/upper-case x))
-    )
-  )
+      (mumble-letter (get char-seq 0) 0))))
